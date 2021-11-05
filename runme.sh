209a6d78bb8e5b211cb0f477f24ec46a23b55a9a
Mahmoud Osman (MLT) <60033959+Mahmoud7Osman@users.noreply.github.com>
2021-11-05 21:31:20 +0200

    Add files via upload

--- runme.sh
+++ runme.sh
@@ -26,17 +26,27 @@ menu="
     ${red}1: ${white}Eliminate Embedded Spaces In Files & Folders Name (src/filename-space-remover)
     ${red}2: ${white}CPU And Memory Information                        (src/hw_info)
     ${red}3: ${white}Enumerate Users Groups IDs                        (src/user-enumerator)
-    ${red}4: ${white}Show Mounted Devices and It's Path                (scr/mounted-device-shower)
-    ${red}5: ${white}Enumerate Users Login Shells                      (scr/users-login-shell)
-    ${red}6: ${white}Execute A Program On System Startup               (scr/add-rc-command)
+    ${red}4: ${white}Show Mounted Devices and It's Path                (src/mounted-device-shower)
+    ${red}5: ${white}Enumerate Users Login Shells                      (src/users-login-shell)
+    ${red}6: ${white}Execute A Program On System Startup               (src/add-rc-command)
+    ${red}7: ${white}Search For A Library Or A Command                 (src/search-similar-packages)
+    ${red}8: ${white}Perform a Symbolic Link On A File                 (src/symbolic-link)
+    ${red}9: ${white}Perform a Hard Link On A File                     (src/hard-link)
+
+    ${green}i: ${white}Open Input To Execute a Command With $SHELL
 
     ${cyan}m: ${white}Print This Menu Again
     ${cyan}b: ${white}Print Program Banner
     ${cyan}r: ${white}Print Banner And Menu Again
     ${cyan}c: ${white}Clear Screen
+    ${cyan}a: ${white}About Me
     ${cyan}q: ${white}Quit This Program
 "
 printf "$menu"
+
+aboutme(){
+    printf "I Am Mahmoud Osman, I Made This Software When I Was 16 Years Old Learning Linux Sysadmin And Working On Some Projects Using C++ etc...,\nI Started Out In 2021, Learned C/C++/Assembly With Binary Exploitation, Moved To Network Socket Programming, Then Into Malware And Software Development...etc, All Of Those Stuffs in 2021\nWhatever Year Your Reading This Aboutme, I Will try To keep This Aboutme Out Of My Reach.\nIn Order to Not Edit It. Goodluck\n"
+}
 loop(){
 printf "\n${white}(${red}SAS${white})${purple} <-:${white} "
 read -n 1 MN
@@ -67,6 +77,29 @@ elif [ "$MN" == 6 ];then
    printf "Enter Program Path Followed By It's Arguments : "
    read cmd
    src/add-rc-command "$cmd"
+elif [ "$MN" == 7 ];then
+   printf "Enter A Package Name, Or A Similar Name To it : "
+   read arg
+   src/search-similar-packages "$arg"
+elif [ "$MN" == 8 ];then
+   printf "Enter Original Filename : "
+   read of
+   printf "Enter File Link Name : "
+   read lf
+   src/symbolic-link "$of" "$lf"
+elif [ "$MN" == 9 ];then
+   printf "Enter Original Filename : "
+   read of
+   printf "Enter File Link Name : "
+   read lf
+   src/hard-link "$of" "$lf"
+
+elif [ "$MN" == 'i' ];then
+   printf "CMD> "
+   read -e cmd
+   "$SHELL" -c "$cmd"
+elif [ "$MN" == 'a' ];then
+   aboutme
 fi
 }
 while [ 1 ];do loop; done
